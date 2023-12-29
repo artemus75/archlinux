8f89adf531140cf589dc59b3877e832c6b8762fc
Michael Melchers <mmelchers75@gmail.com>
2023-12-29 12:01:44 +0100

    new file for Homebrew installation

---
+++ 160-homebrew-v1.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author 	: 	Michael Melchers
+##################################################################################################################
+
+echo "----------------"
+echo "Install Homebrew"
+echo "----------------"
+
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+test -d ~/.linuxbrew && eval "$(~/.linuxbrew/bin/brew shellenv)"
+test -d /home/linuxbrew/.linuxbrew && eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+echo "eval \"\$($(brew --prefix)/bin/brew shellenv)\"" >> ~/.zshrc
+
+echo "---------------------------------------------------"
+echo "Check installation of Homebrew by displaying the version"
+echo "---------------------------------------------------"
+
+brew --version
